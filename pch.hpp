9811dc73c1c944cad54c9f12fb73b7d7420ba847
mzaid <i253088@isb.nu.edu.pk>
2026-06-13 21:57:46 +0200

    Add files via upload

---
+++ pch.hpp
@@ -0,0 +1,13 @@
+#ifndef PCH_HPP
+#define PCH_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+#endif
